d62c0f83fbff11c5bee2fa73530ea4fba20217fa
Nikita Boiko <genertyniki@gmail.com>
2019-04-01 16:59:11 +0300

    add db-call methods

--- app/src/main/java/ru/nikitaboiko/stoservice/MainActivity.kt
+++ app/src/main/java/ru/nikitaboiko/stoservice/MainActivity.kt
@@ -1,12 +1,32 @@
 package ru.nikitaboiko.stoservice
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import ru.nikitaboiko.stoservice.fragments.UserRegFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), UserRegFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userRegButton = findViewById<View>(R.id.activity_main_button_user_reg)
+        val addService = findViewById<View>(R.id.activity_main_button_add_service)
+        val serviceList = findViewById<View>(R.id.activity_main_button_services_list)
+
+        userRegButton.setOnClickListener {
+            var fragment = UserRegFragment()
+            var fTransaction = supportFragmentManager.beginTransaction()
+            fTransaction.add(R.id.activity_main, fragment)
+            fTransaction.commit()
+
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/ru/nikitaboiko/stoservice/fragments/UserRegFragment.kt
@@ -0,0 +1,105 @@
+package ru.nikitaboiko.stoservice.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_user_reg.view.*
+import org.apache.commons.codec.binary.Hex
+import org.apache.commons.codec.digest.DigestUtils
+import ru.nikitaboiko.stoservice.App
+import ru.nikitaboiko.stoservice.R
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class UserRegFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+    lateinit var login: TextView
+    lateinit var password: TextView
+    lateinit var passwordRepeat: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val inflatedView = inflater.inflate(R.layout.fragment_user_reg, container, false)
+        val buttonAdd = inflatedView.fragment_user_reg_button_add
+        login = inflatedView.fragment_user_reg_login
+        password = inflatedView.fragment_user_reg_password
+        passwordRepeat = inflatedView.fragment_user_reg_password_repeat
+
+        buttonAdd.setOnClickListener {
+            addUser()
+        }
+
+        return inflatedView
+    }
+
+    private fun addUser() {
+        if (password.text.toString().equals(passwordRepeat.text.toString())) {
+            val passMd5 = Hex.encodeHex(DigestUtils.md5("vicomlite" + password.text.toString())).toString()
+            Toast.makeText(
+                App.instance()?.baseContext,
+                "MD5 ${passMd5}",
+                Toast.LENGTH_LONG
+            ).show()
+            //DigestUtils.md5Hex("vicomlite" + password.text.toString())
+            //App.instance()?.dataControl?.addUser(login.text.toString(), passMd5)
+        } else {
+            Toast.makeText(
+                App.instance()?.baseContext,
+                "Пароли не совпадают ${password.text.toString()} and ${passwordRepeat.text.toString()}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            UserRegFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
