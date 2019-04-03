5f11f5494ccab01b299b9b28c233ff877454e5de
NikiArt <genertyniki@gmail.com>
2019-04-03 21:40:10 +0300

    add date dialog

--- app/src/main/java/ru/nikitaboiko/stoservice/WorkerActivity.kt
+++ app/src/main/java/ru/nikitaboiko/stoservice/WorkerActivity.kt
@@ -2,18 +2,34 @@ package ru.nikitaboiko.stoservice
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import ru.nikitaboiko.stoservice.fragments.DateDialog
+import java.text.SimpleDateFormat
+import java.util.*
 
 class WorkerActivity : AppCompatActivity() {
-
+    lateinit var priceDate: TextView
+    val sdf = SimpleDateFormat("01 MMMM y")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_worker)
         val displaymetrics = resources.displayMetrics
         val addWorkButton = findViewById<View>(R.id.activity_worker_button_addwork)
-        val priceDate = findViewById<View>(R.id.activity_worker_date_price)
+        priceDate = findViewById<View>(R.id.activity_worker_date_price) as TextView
         val workList = findViewById<View>(R.id.activity_worker_work_list)
-        workList.layoutParams.height = displaymetrics.heightPixels / 2
+        priceDate.text = sdf.format(Date())
+        priceDate.setOnClickListener {
+            openDateDialog()
+        }
+    }
 
+    private fun openDateDialog() {
+        val manager = supportFragmentManager
+        val myDialogFragment = DateDialog()
+        val bundle = Bundle()
+        bundle.putString("startDate", priceDate.text.toString())
+        myDialogFragment.arguments = bundle
+        myDialogFragment.show(manager, "dialog")
     }
 }
